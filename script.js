e35d459567ea31d2a31bebae57af9f249ebb020b
Tej370 <153205267+Tej370@users.noreply.github.com>
2024-07-03 23:29:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function toCase(text) {
   // write your code here
-const lowercaseText = text.toLowerCase();
-    // Convert the original text to uppercase
-    const uppercaseText = text.toUpperCase();
-    // Concatenate them with a hyphen as the delimiter
-    const result = `${lowercaseText}-${uppercaseText}`;
-    // Return the final result
-    return result;
+let a = text.toLowerCase();
+let b = text.toUpperCase();
+let c = (a) "-" (b);
+return c;
 	
 }
 
